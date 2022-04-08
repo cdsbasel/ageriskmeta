94f33a12558997c563cc1bcc0d97bdea41add278
YR <Yunrui.liu@unibas.ch>
2022-04-08 13:46:11 +0200

    Code

---
+++ Code/Preprocess_code/Preprocess_GLES.R
@@ -0,0 +1,141 @@
+### GLES Panel 2016-2021 Prprocess ###
+
+rm(list=ls())
+
+### kpx_1572: General risk
+# survey time
+# W1: kp1_1572, October-November 2016
+# A1 Aufstocker: July-August 2017
+# W13: April-June 2020
+# A2 Profile Wave 2020: September-November 2020
+# W14: November-December 2020
+# W15: February-March 2021
+
+### ID
+# lfdn: Laufende Number (Running number)(Respodent id)
+# study: studiennummer (study number)(Study id)
+
+### Demographic
+# kpx_2280: Geschlecht (Gender)
+# kpx_2290: year or birth  (only find kpx_2290s in W1)
+# kpx_2291: month of birth (only find kpx_2290flag and kpx_2291flag, kpx_2290flag == kpx_2291flag)
+
+
+### Date:
+# W1: kp1_datetime
+# A1 Aufstocker: kpa1_datetime
+# W13: kp13_datetime
+# A2 Profile Wave 2020: kpa2_datetime
+# W14: kp14_datetime
+# W15: kp15_datetime
+
+### load packages
+library(foreign)
+library(tidyverse)
+library(naniar)
+
+path = "~/Desktop/analysis/meta_analysis_risk_normalized_5/rawdata/GLES_data/sav"   # sav. format dataset does not include a1, but there is something wrong about the csv. format file;
+setwd(path)
+
+W1 <- read.spss("ZA6838_w1to9_sA_v5-0-0.sav", to.data.frame = T, use.value.labels = F, reencode=F)  %>%
+  select(study, lfdn, kpx_2280, kpx_2290s, kpx_2291flag, kp1_1572, kp1_datetime) %>%
+  rename(ID=lfdn, Gender=kpx_2280, birth_yr=kpx_2290s, birth_mo=kpx_2291flag, intdate=kp1_datetime, risk=kp1_1572) %>%
+  mutate(birth_date = as.Date(paste(birth_mo, 15, birth_yr, sep="/"), "%m/%d/%Y")) %>%
+  mutate(int_date = as.Date(intdate,format='%Y-%m-%d')) %>%
+  mutate(age = round((as.numeric(int_date - birth_date))/365)) %>%
+  mutate(SYEAR = 1)
+  
+
+
+W13 <- read.spss("ZA6838_w13_sA_v5-0-0.sav", to.data.frame = T, use.value.labels = F, reencode=F) %>%
+  select(study, lfdn, kpx_2280, kpx_2290s, kpx_2291flag, kp13_1572, kp13_datetime) %>%
+  rename(ID=lfdn, Gender=kpx_2280, birth_yr=kpx_2290s, birth_mo=kpx_2291flag, intdate=kp13_datetime, risk=kp13_1572) %>%
+  mutate(birth_date = as.Date(paste(birth_mo, 15, birth_yr, sep="/"), "%m/%d/%Y")) %>%
+  mutate(int_date = as.Date(intdate,format='%Y-%m-%d')) %>%
+  mutate(age = round((as.numeric(int_date - birth_date))/365)) %>%
+  mutate(SYEAR = 13)
+  
+
+
+W14 <- read.spss("ZA6838_w14_sA_v5-0-0.sav", to.data.frame = T, use.value.labels = F, reencode=F) %>%
+  select(study, lfdn, kpx_2280, kpx_2290s, kpx_2291flag, kp14_1572, kp14_datetime) %>%
+  rename(ID=lfdn, Gender=kpx_2280, birth_yr=kpx_2290s, birth_mo=kpx_2291flag, intdate=kp14_datetime, risk=kp14_1572) %>%
+  mutate(birth_date = as.Date(paste(birth_mo, 15, birth_yr, sep="/"), "%m/%d/%Y")) %>%
+  mutate(int_date = as.Date(intdate,format='%Y-%m-%d')) %>%
+  mutate(age = round((as.numeric(int_date - birth_date))/365)) %>%
+  mutate(SYEAR = 14)
+  
+
+W15 <- read.spss("ZA6838_w15_sA_v5-0-0.sav", to.data.frame = T, use.value.labels = F, reencode=F) %>%
+  select(study, lfdn, kpx_2280, kpx_2290s, kpx_2291flag, kp15_1572, kp15_datetime) %>%
+  rename(ID=lfdn, Gender=kpx_2280, birth_yr=kpx_2290s, birth_mo=kpx_2291flag, intdate=kp15_datetime, risk=kp15_1572) %>%
+  mutate(birth_date = as.Date(paste(birth_mo, 15, birth_yr, sep="/"), "%m/%d/%Y")) %>%
+  mutate(int_date = as.Date(intdate,format='%Y-%m-%d')) %>%
+  mutate(age = round((as.numeric(int_date - birth_date))/365)) %>%
+  mutate(SYEAR = 15)
+  
+
+
+GLES_preprocess <- Reduce(function(x, y) merge(x, y, all=TRUE), list(W1, W13, W14, W15)) %>%
+  select(ID, SYEAR, Gender, age, risk) %>%
+  rename(General_raw = risk) %>%
+  replace_with_na(replace = list(General_raw = c(-93, -95, -99))) %>%
+  mutate(General=10*(General_raw-1)/(11-1)) %>%
+  filter(!is.na(age)) %>%
+  filter(!is.na(Gender)) %>%
+  filter(age >= 18 & age <= 90) 
+  
+
+
+### check Gender and delete participants have different Gender
+GLES_ID_Gender <- GLES_preprocess %>%
+  dplyr::select(ID, Gender)
+
+unique_GLES_ID_Gender <-  unique(GLES_ID_Gender) # get unique rows: unique function can eliminate duplicate rows
+dupIDs <- with(unique_GLES_ID_Gender, ID[duplicated(ID)])  # duplicate IDs, which means different Gender: duplicated function can be used to identify duplicate ids after duplicate rows are removed
+errorGLES <- subset(GLES_preprocess, ID %in% dupIDs)  
+rightGLES <- subset(GLES_preprocess, !(ID %in% dupIDs)) 
+
+### just keep participants >=2 records
+dup_GLES <- rightGLES %>%
+  dplyr::select(ID, age, Gender, SYEAR) %>%
+  filter(!is.na(Gender)) %>%
+  group_by(ID) %>%
+  filter(n()>=2) %>%
+  ungroup()
+
+dup_GLES_ID <- unique(dup_GLES$ID)   # Participant ID have duplicate records
+GLES_process1 <- subset(rightGLES, !(ID %in% dup_GLES_ID)) # Participant have <2 records
+GLES_process2 <- subset(rightGLES, (ID %in% dup_GLES_ID))  # Participant have >=2 records
+
+### cleaned data
+GLES_process <- GLES_process2
+
+
+### save 
+setwd("~/Desktop/analysis/meta_analysis_risk_normalized_5/data/process_data")
+saveRDS(GLES_process, file = "GLES_process.Rds")
+
+
+
+### set age group
+agebins = c(18, seq(25, 90, 5))
+GLES_process$age_bins <- cut(GLES_process$age, breaks=agebins, include.lowest = TRUE, right = TRUE, lable = TRUE)
+
+GLES_process_reference <- GLES_process %>%
+  filter(age_bins=="(50,55]")
+
+mean_General <- mean(GLES_process_reference$General, na.rm = TRUE)
+sd_General <- sd(GLES_process_reference$General, na.rm = TRUE)
+
+GLES_process_z <- GLES_process %>%
+  mutate(General_z = (General - mean_General)/sd_General)
+
+### save and load
+setwd("~/Desktop/analysis/meta_analysis_risk_normalized_5/data/process_z_data")
+saveRDS(GLES_process_z, file = "GLES_process_z.Rds")
+
+#GLES_process_z <- readRDS(file = "GLES_process_z.Rds")
+
+  
+
---
+++ Code/Visualization_code/plot_trajectory_all.R
@@ -0,0 +1,131 @@
+### Visualization ###
+rm(list=ls())
+library(metafor)
+library(lme4)
+library(tidyverse)
+library(ggplot2)
+library(gridExtra)
+library(ggpubr)
+library(grid)
+library(RColorBrewer)
+library(patchwork)
+
+setwd("../data/results_data")
+
+### prepare some loop variable ###
+panels = c("DHS", "GCOE_Japan", "GCOE_USA", "HILDA", "HRS", "LIKS", "PHF", "SAVE", 
+           "SHARE_Austria", "SHARE_Belgium", "SHARE_Czech_Republic", "SHARE_Denmark", "SHARE_Estonia", "SHARE_France", "SHARE_Germany","SHARE_Israel", "SHARE_Italy", "SHARE_Netherlands", "SHARE_Slovenia", "SHARE_Spain", "SHARE_Sweden", "SHARE_Switzerland", 
+           "SOEP","USoc")
+domains = c("General", "Financial", "Driving", "Recreational", "Occupational", "Health", "Social")
+ab_domains = c("g", "f", "d", "r", "o", "h", "s")
+models = c("int", "fixed", "lg", "lg.gender", "lg.inter", "quad", "quad.gender")
+model_plot = models[3:7]
+
+
+### function1: combine exiting data and create a data list
+combine_data <- function(ab_domains, model_plot, data_list){
+  for (b in 1:length(ab_domains)) {
+    for (c in 1: length(model_plot)) {
+      plot_name = paste0("plot_",ab_domains[b],".", model_plot[c])
+      sub_data = data_list[[plot_name]] 
+      sub_data = sub_data[vapply(sub_data, Negate(is.null), NA)]
+      if(length(sub_data) > 0) {
+        for(i in 1:length(sub_data)){
+          name_list = names(sub_data)
+          sub_data[[i]]$Panel = name_list[i]
+          sub_data[[i]]$Panel = as.factor(sub_data[[i]]$Panel)
+        }
+        sub_data = Reduce(function(x, y) merge(x, y, all=TRUE), sub_data)
+      }
+      data_list[[plot_name]] = sub_data
+    }
+  }
+  return(data_list)
+}
+
+### load model predict data
+plot_model_data_list = readRDS(file = "plot_data_results_list.Rds")
+
+### combine existing data and create_model_data 
+plot_model_data_list = combine_data(ab_domains, model_plot, data_list=plot_model_data_list)
+
+### save plot_model_data_list for plot
+#saveRDS(plot_model_data_list, file ="plot_model_data_list.Rds")
+
+
+###function2: create a empty figure_list for saving all plot
+c=2
+index=0
+plot_name_list = list()  
+figure_list = vector("list", length = length(ab_domains))
+for (b in 1:length(ab_domains)){
+  index = index+1
+  plot_name_list[[index]] = paste0("plot_",ab_domains[b],".", model_plot[c])
+}
+names(figure_list) = plot_name_list
+
+
+index = c("DHS","GCOE_Japan","GCOE_USA","HILDA","HRS","LIKS","PHF","SAVE","SHARE_Austria","SHARE_Belgium","SHARE_Czech_Republic","SHARE_Denmark", "SHARE_Estonia", "SHARE_France", "SHARE_Germany","SHARE_Israel", "SHARE_Italy","SHARE_Netherlands","SHARE_Slovenia","SHARE_Spain","SHARE_Sweden","SHARE_Switzerland","SOEP","USoc")  
+values = c("DHS","GCOE","GCOE","HILDA","HRS","LIKS","PHF","SAVE","SHARE","SHARE","SHARE","SHARE", "SHARE", "SHARE", "SHARE","SHARE", "SHARE","SHARE","SHARE","SHARE","SHARE","SHARE","SOEP","USoc") 
+lines = c(rep("a", 8), rep("c", 14), rep("a", 2))
+
+colordata = data.frame(matrix(data=NA, nrow=length(panels), ncol = 1)) 
+colordata = as.data.frame(matrix(panels))
+colnames(colordata) = "Sample"
+colordata$Panel = values[match(colordata$Sample, index)]
+colordata$Lines = lines[match(colordata$Sample, index)]
+colordata$Sample = as.factor(colordata$Sample) 
+colordata$Panel = as.factor(colordata$Panel) 
+colordata$Lines = as.factor(colordata$Lines) 
+
+
+# ### plot: plot all panels in one picture (same y scale)
+for (b in 1:length(ab_domains)) {
+  y_name = paste0(domains[b],"_z_mean")
+  y_number = paste0(domains[b],"_z_n")
+  y_domain = domains[b]
+  plot_name = paste0("plot_",ab_domains[b],".", model_plot[c])
+
+  #model data
+  model_data = plot_model_data_list[[plot_name]]
+  names(model_data)[names(model_data) == "Panel"] = "Sample"
+  model_data$Panel = values[match(model_data$Sample, index)]
+  model_data$Lines = lines[match(model_data$Sample, index)]
+  
+  model_data[,"gender"] = ifelse(model_data$gender == 0, "Male", "Female")
+  model_data$gender = factor(model_data$gender, levels = c("Male", "Female"))
+  
+  
+  figure_list[[plot_name]] <- ggplot() + 
+    stat_smooth(data = model_data, aes(x = age, y = fit, colour=Sample, linetype=Sample), se=FALSE, method = "lm", formula = y ~ x, size = 0.5) +
+    scale_colour_discrete(drop=FALSE, limits = levels(colordata$Sample)) +
+    scale_linetype_manual(limits = levels(colordata$Sample), values = c(rep("solid", 8), rep("dashed", 14), rep("solid", 2))) + 
+    stat_smooth(data = model_data, aes(x = age, y = fit), color = "black",se=TRUE, method = "lm", formula = y ~ x, size = 1.1) +
+    labs(x= "Age", y= paste(y_domain, "risk-taking", sep = " ")) +
+    scale_x_continuous(breaks = seq(20, 90, 10))+
+    coord_cartesian(ylim = c(-1, 1)) +
+    theme_bw() +
+    theme(panel.border = element_rect(colour = "black", fill=NA))+
+    facet_wrap(~gender, nrow=1, scales="free_x") +
+    guides(col = guide_legend(ncol = 1))
+}
+
+General = figure_list$plot_g.lg.gender
+Financial = figure_list$plot_f.lg.gender
+Driving = figure_list$plot_d.lg.gender 
+Recreational = figure_list$plot_r.lg.gender
+Occupational = figure_list$plot_o.lg.gender
+Health = figure_list$plot_h.lg.gender
+
+
+(General + theme(plot.margin = unit(c(0,20,0,0), "pt"))) /
+  (Financial + theme(plot.margin = unit(c(0,20,0,0), "pt")))/
+  (Driving + theme(plot.margin = unit(c(10,20,10,0), "pt"))) / 
+  (Recreational + theme(plot.margin = unit(c(10,20,10,0), "pt")))/
+  (Occupational + theme(plot.margin = unit(c(0,20,0,0), "pt")))/
+  (Health + theme(plot.margin = unit(c(0,20,0,0), "pt"))) +
+  plot_layout(guides = 'collect', ncol = 2) +
+  plot_annotation(tag_levels = 'a') & theme(plot.tag = element_text(face = 'bold')) 
+
+
+
